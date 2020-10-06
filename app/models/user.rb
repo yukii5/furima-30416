41f6aebff2dda85726477b7f52f70158748f82ef
yukii5 <assault03@gmail.com>
2020-10-06 20:34:18 +0900

    with_options

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,22 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
 
-  validates :nickname,               presence: true
+  with_options presence: true do
+   
 
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力して下さい' }
+  validates :nickname
 
-  validates :first_name,  presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力して下さい' }
+  validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力して下さい' }
 
-  validates :family_name_furikana, presence: true,format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: 'は全角カタカナのみで入力して下さい' } 
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力して下さい' }
 
-  validates :first_name_furikana, presence: true,format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,message: 'は全角カタカナのみで入力して下 さい' } 
+  validates :family_name_furikana, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: 'は全角カタカナのみで入力して下さい' } 
 
+  validates :first_name_furikana, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,message: 'は全角カタカナのみで入力して下 さい' } 
+
+  end
   validates :birthday,               presence: true
 
   has_many :items
