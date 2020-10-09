e15d14eed5fe311e9e9836cf0d20e82f536c6fb6
yukii5 <assault03@gmail.com>
2020-10-09 18:26:02 +0900

    手数料修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -3,17 +3,19 @@
   const add_tax = document.getElementById("add-tax-price");
   const profit = document.getElementById("profit");
     priceInput.addEventListener('keyup', () => {
-        const value = priceInput.value;
+        const value = priceInput.value
         
       if (value >= 300 && value <= 9999999){
         let fee = value * 0.1
-        let gains = value - fee}
-      else {
+        let gains = value - fee
+        add_tax.textContent = fee;
+        profit.textContent = gains;
+    } else {
       let fee = '-';
-      let gains = '-';};
+      let gains = '-';
       add_tax.textContent = fee;
       profit.textContent = gains;
-    
+    }
   });
 }
 window.addEventListener('load', price);
