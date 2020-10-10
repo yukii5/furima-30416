c114b0fdeebaf3ec8248ce99fdd40cdee64e020e
yukii5 <assault03@gmail.com>
2020-10-10 10:16:24 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,11 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new    
   end
+
+  def show  
+    @item = Item.find(params[:id])
+  end
+  
   
   def create
     @item = Item.new(items_params)
