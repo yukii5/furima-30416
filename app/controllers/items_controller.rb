984a258364acec236481d1d8d28d283c6df8ff29
yukii5 <assault03@gmail.com>
2020-10-13 19:45:49 +0900

    リダイレクト修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,8 @@ class ItemsController < ApplicationController
   
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
+
   
   def index
     @items = Item.includes(:user).order('created_at DESC') 
@@ -54,4 +56,11 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
+
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :current_user
   before_action :set_item, only: [:index, :edit, :update, :pay_item, :create]
   before_action :move_to_index, only: [:index]
+  before_action :authenticate_user!,onry: [:index, :show]
 
   def index
     @address = AddressForm.new
@@ -22,7 +23,6 @@ class OrdersController < ApplicationController
   private
   
   def order_params
-    # binding.pry
     params.permit(:postal_code, :region_id, :municipalities, :house_number, :building_name, :phone_number, :price).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id], order_id: params[:order_id])
   end
   
